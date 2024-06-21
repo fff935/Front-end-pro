a66f67efbb758001b247b4944e293a70412c7736
Oleksandr <sasha2004917@gmail.com>
2024-06-21 11:25:43 +0200

    improvement

--- HT4/4.2/numberCheck.js
+++ HT4/4.2/numberCheck.js
@@ -3,7 +3,6 @@ let allSameNumer;
 let twoSameNumber;
 
 if (number % 111 === 0) {
-    //alert(`${number} all digits are the same`);
     allSameNumer = 1;
 }
 else if (new Set(number).size !== number.length) {
@@ -13,9 +12,9 @@ else {
     alert(`"${number}": NO same digits`);
 }
 
-if (allSameNumer == 1){
+if (allSameNumer == 1) {
     alert(`"${number}": all digits are the same`);
 }
-if (twoSameNumber == 1){
+if (twoSameNumber == 1) {
     alert(`Two numbers are the same`);
 }
