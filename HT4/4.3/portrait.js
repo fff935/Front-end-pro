9f05be4ee7136a40ea5cc0292c2868f59d5c6d75
Oleksandr <sasha2004917@gmail.com>
2024-06-21 17:40:02 +0200

    Refactor user input handling and message display logic

--- HT4/4.3/portrait.js
+++ HT4/4.3/portrait.js
@@ -1,65 +1,66 @@
-const birthYear = prompt("Enter your year of birth: ");
-const town = prompt("Enter your town: ");
-let country;
-let captital = false;
-let inCaptital;
-const favSport = prompt("Enter your favorite kind of sport: ");
-let champion;
 const year = 2024;
-let age = year - birthYear;
-//------------------------------------------------------------------
-if (town === "Kyiv") {
-    country = "Ukraine";
-    captital = true;
-} else if (town === "Washington") {
-    country = "USA";
-    captital = true;
-} else if (town === "London") {
-    country = "Great Britain";
-    captital = true;
-}
-
-if (captital) {
-    inCaptital = `You live in the capital of ${country}`;
-} else {
-    inCaptital = `You live in ${town}`;
-}
-//------------------------------------------------------------------
-let rSport;
-let tSport;
-
-if (favSport == "Football") {
-    champion = "Ronaldo";
-    rSport = true;
-}
-else if (favSport == "climbing") {
-    champion = "Alex Megos";
-    rSport = true;
-}
-else if (favSport == "f1") {
-    champion = "Max Verstappen";
-    rSport = true;
+let birthYear = prompt("Enter your year of birth: ");
+if (birthYear === null) {
+    alert("Шкода, що Ви не захотіли ввести свою дату народження.");
 }
 else {
-    rSport = false;
-}
+    let town = prompt("Enter your town: ");
+    if (town === null) {
+        alert("Шкода, що Ви не захотіли ввести своє місто.");
+    } else {
+        let country;
+        let captital = false;
+        let inCaptital;
+        let favSport = prompt("Enter your favorite kind of sport: ");
+        if (favSport === null) {
+            alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
+        }
+        else {
+            let champion;
+            let age = year - birthYear;
+            //------------------------------------------------------------------
+            if (town === "Kyiv") {
+                country = "Ukraine";
+                captital = true;
+            } else if (town === "Washington") {
+                country = "USA";
+                captital = true;
+            } else if (town === "London") {
+                country = "Great Britain";
+                captital = true;
+            }
 
-if (rSport) {
-    tSport = `Cool! Do you want to become a ${champion}?`;
-}
-else {
-    tSport = "You are doing a very interesting sport";
-}
-//------------------------------------------------------------------
-let result = confirm();
+            if (captital) {
+                inCaptital = `You live in the capital of ${country}`;
+            } else {
+                inCaptital = `You live in ${town}`;
+            }
+            //------------------------------------------------------------------
+            let rSport;
+            let tSport;
 
-if (result){
-    console.log("ok");
-}
-else{
-    console.log("cancel");
-}
+            if (favSport === "Football") {
+                champion = "Ronaldo";
+                rSport = true;
+            } else if (favSport === "climbing") {
+                champion = "Alex Megos";
+                rSport = true;
+            } else if (favSport === "f1") {
+                champion = "Max Verstappen";
+                rSport = true;
+            } else {
+                rSport = false;
+            }
 
-let message = `You're about ${age} years old\n${inCaptital}\n${tSport}`;
-alert(message);
+            if (rSport) {
+                tSport = `Cool! Do you want to become a ${champion}?`;
+            } else {
+                tSport = "You are doing a very interesting sport";
+            }
+            //------------------------------------------------------------------
 
+            let message = `You're about ${age} years old\n${inCaptital}\n${tSport}`;
+            alert(message);
+        }
+    }
+}
