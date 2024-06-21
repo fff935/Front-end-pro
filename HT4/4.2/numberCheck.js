ddefc9cfdb624a1fed7d727fceeab5f445a82c06
Oleksandr <sasha2004917@gmail.com>
2024-06-21 11:22:05 +0200

    Add HTML and JavaScript files for number checking

---
+++ HT4/4.2/numberCheck.js
@@ -0,0 +1,21 @@
+let number = prompt("Enter your number:");
+let allSameNumer;
+let twoSameNumber;
+
+if (number % 111 === 0) {
+    //alert(`${number} all digits are the same`);
+    allSameNumer = 1;
+}
+else if (new Set(number).size !== number.length) {
+    twoSameNumber = 1;
+}
+else {
+    alert(`"${number}": NO same digits`);
+}
+
+if (allSameNumer == 1){
+    alert(`"${number}": all digits are the same`);
+}
+if (twoSameNumber == 1){
+    alert(`Two numbers are the same`);
+}
