cc532acd31ad7d4d810b10b0169317a316d011ba
Oleksandr <sasha2004917@gmail.com>
2024-07-15 15:37:42 +0200

    Add HTML and JavaScript files for salary sum calculation

---
+++ HT9/9.1/salary-sum.js
@@ -0,0 +1,34 @@
+let company = {
+    sales: [
+      { name: "John", salary: 1000 },
+      { name: "Alice", salary: 600 },
+    ],
+    development: {
+      web: [
+        { name: "Peter", salary: 2000 },
+        { name: "Alex", salary: 1800 },
+      ],
+      internals: [{ name: "Jack", salary: 1300 }],
+    },
+  };
+  
+  function calculateTotalSalary(department) {
+    let total = 0;
+  
+    if (Array.isArray(department)) {
+      for (let worker of department) {
+        total += worker.salary;
+      }
+    } else {
+      for (let subDepartment in department) {
+        total += calculateTotalSalary(department[subDepartment]);
+      }
+    }
+  
+    return total;
+  }
+  
+  let totalSalary = calculateTotalSalary(company);
+  
+  console.log(totalSalary);
+  
