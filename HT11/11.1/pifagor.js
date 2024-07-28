ff8c3946443d26ad1ea04ff9705ffb15bc2d68e1
Oleksandr <sasha2004917@gmail.com>
2024-07-28 11:00:15 +0200

    Add HTML and JavaScript files for multiplication table

---
+++ HT11/11.1/pifagor.js
@@ -0,0 +1,9 @@
+document.write('<table border="1">');
+for(let i = 1; i <=10; i++){
+    document.write('<tr>')
+    for(let j = 1; j <= 10; j++){
+        document.write('<td>' + i * j + '</td>');
+    }
+    document.write('</tr>');
+}
+document.write('</table>');
