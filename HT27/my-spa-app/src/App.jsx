f7cb3526528e05ced237453ae34549b268106539
Oleksandr <sasha2004917@gmail.com>
2024-11-10 13:36:46 +0100

    Add NotFound component for handling invalid routes

--- HT27/my-spa-app/src/App.jsx
+++ HT27/my-spa-app/src/App.jsx
@@ -1,11 +1,12 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import ThemeToggle from './components/themeToggle';
+import ThemeToggle from './components/ThemeToggle';
 import ErrorBoundary from './components/ErrorBoundary';
 import Home from './components/Home';
 import Contacts from './components/Contacts';
 import About from './components/About';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/about" element={<About />} />
+          {/* Додайте цей маршрут для всіх невірних посилань */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </ErrorBoundary>
     </Router>
---
+++ HT27/my-spa-app/src/components/NotFound.jsx
@@ -0,0 +1,7 @@
+// src/components/NotFound.jsx
+function NotFound() {
+    return <h2>Сторінку не знайдено. Перевірте URL.</h2>;
+  }
+  
+  export default NotFound;
+  
